46cbc1c3b76538864961a8ab55f1150bc81dae75
ignismeow <saadbeenco@gmail.com>
2024-12-20 13:46:38 +0500

    app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,24 +21,24 @@ function App() {
 
   return (
     <>
-      <div className="bg-[#282725] h-[100vh] flex flex-col justify-center items-center text-center font-[inter]">
+      <div className="bg-[#282725] h-[100vh] flex flex-col justify-center items-center text-center font-[inter] ">
         <header className="max-w-[60vw]">
           <div className="">
             <h1 className="text-[#F9F4DA] font-[inter] font-bold text-3xl">Assembly: Endgame</h1>
             <p className="text-[#8E8E8E] font-[inter] text-[1rem] pt-3">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
             </div>
+        </header>
+        <main className="max-w-[50vw] min-w-[400px]">
           <section className="status-section">
             <div className="bg-[#10A95B] text-white rounded-md py-4 my-10 font-normal text-xl">
               <p>You win!</p>
               <p className="font-light">Well done!🎉</p>
             </div>
           </section>
-        </header>
-        <main className="max-w-[60vw]">
           <section className="flex flex-wrap justify-center">
             {mapCurrentWord}
           </section>
-          <section className="programmingLanguages-section flex gap-1 flex-wrap justify-center my-10">
+          <section className="programmingLanguages-section flex gap-1 flex-wrap justify-center my-10 ">
              {allLanguages}
           </section>
           <section className="programmingLanguages-section flex gap-1 flex-wrap justify-center">
